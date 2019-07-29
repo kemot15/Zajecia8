892e19705edfdf4c4b2b893a2c596253c780c245
Tomek <kemot15@tlen.pl>
2019-07-29 11:33:24 +0200

    Zad 9 ver 1

--- src/Zad9/ChristmasTree.java
+++ src/Zad9/ChristmasTree.java
@@ -3,15 +3,18 @@ package Zad9;
 import java.util.Scanner;
 
 public class ChristmasTree {
-    public void paintTree(){
+    public void printTree(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj liczbe poziomow");
         int level = scanner.nextInt();
         scanner.nextLine();
-        String[][] tree = new String[level][];
-        level = (level-1)*2+1;
-        for (int i = tree.length-1; i >= 0; i++){
-            tree[i][] = new tree[i][level];
+        for (int i = level-1; i>=0; i--){
+            int stars = (level-i)*2-1;
+            for (int j = 0; j<i; j++)
+                System.out.print(" ");
+            for (int j = 0; j<stars; j++)
+                System.out.print("*");
+            System.out.println();
         }
     }
 }
---
+++ src/Zad9/ChristmasTreeTest.java
@@ -0,0 +1,9 @@
+package Zad9;
+
+public class ChristmasTreeTest {
+    public static void main(String[] args) {
+        ChristmasTree christmasTree = new ChristmasTree();
+        christmasTree.printTree();
+    }
+
+}
